b03028db426f3f374942ce1a9250824d1397af92
Alfred-Lau <jian.liu@icloud.com>
2018-02-09 09:23:09 +0800

    add react-router

---
+++ app/containers/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component{
+    render() {
+        return (
+            <div>
+                hhahaa
+            </div>
+        );
+    }
+}
+
+export default Home;
--- app/index.jsx
+++ app/index.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
+import Home from './containers/Home';
 import './index.less';
+import Detail from './containers/Detail';
+import List from './containers/List';
 
 class App extends React.Component {
-    render() {
+    render () {
         return (
-            <div className='wrapper'>
-                hello,react.
-            </div>
+            <Router>
+                <div className='wrapper'>
+                    <ul>
+                        <li>
+                            <Link to='/'> Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/list'> List
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/detail'> Detail
+                            </Link>
+                        </li>
+                    </ul>
+                    <Route exact path='/' component={Home} />
+                    <Route path='/detail' component={Detail} />
+                    <Route path='/list' component={List} />
+                </div>
+            </Router>
         );
     }
 }
 
 render(<App />, document.getElementById('root'));
-
